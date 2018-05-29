6601db4408f8e51a1a8c8300aa9bd738ac50e97f
Your Name <charlzjin@gmail.com>
2018-05-29 07:04:03 -0400

    Add PE002_Julia.jl

--- PE001_Julia.jl
+++ PE001_Julia.jl
@@ -1,3 +1,9 @@
+#=
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+=#
+
 ans = sum(filter(a -> a % 3 == 0 || a % 5 == 0, (x for x in 1:999)))
 
 println(ans)
